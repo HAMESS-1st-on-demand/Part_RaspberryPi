5461c1ec7ea58a44a950b5542aef86c9274331b1
js_whi <sangwhi127@naver.com>
2023-10-23 17:12:31 +0900

    uart 송신 코드 작성

--- termiosend.c
+++ termiosend.c
@@ -8,7 +8,7 @@
 
 int main() {
     // chmod를 변경하는 코드
-    int setting = system("sudo chmod 666 /dev/ttyS0");
+    int setting = system("sudo chmod 666 /dev/ttyAMA0");
 
     if(setting == -1)
     {
@@ -19,7 +19,7 @@ int main() {
     struct termios options; 
 
     // UART 포트 열기
-    uart_fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY); // | O_NDELAY); // O_RDWR : 읽고 쓰기, O_NOCTTY : 키보드 중단 신호 영향, O_NDELAY : 수신 대기 차단 
+    uart_fd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY); // | O_NDELAY); // O_RDWR : 읽고 쓰기, O_NOCTTY : 키보드 중단 신호 영향, O_NDELAY : 수신 대기 차단 
     if (uart_fd == -1) {
         perror("Error - Unable to open UART");
         return 1;
@@ -27,7 +27,7 @@ int main() {
 
     // 통신 설정 가져오기
     tcgetattr(uart_fd, &options);
-    options.c_cflag = B9600 | CS5 | CLOCAL | CREAD; // Baud rate 설정: 9600, 5-bit, 지역 연결, 데이터 수신 허용 (bit 5 ~ 8)
+    options.c_cflag = B9600 | CS8 | CLOCAL | CREAD; // Baud rate 설정: 9600, 5-bit, 지역 연결, 데이터 수신 허용 (bit 5 ~ 8)
     options.c_iflag = IGNPAR; // Parity 오류 무시
     options.c_oflag = 0;
     options.c_lflag = 0;
@@ -40,11 +40,17 @@ int main() {
     tcflush(uart_fd, TCIFLUSH);
     tcsetattr(uart_fd, TCSANOW, &options);
 
+    int vv = 0;
     // 데이터 송신
-    char tx_buffer[256];
-    strcpy(tx_buffer, "Hello from UART!\n");
-    if (write(uart_fd, tx_buffer, strlen(tx_buffer)) < 0) {
-        perror("UART TX error");
+    while(1)
+    {
+        char tx_buffer[256] = {0};
+        sprintf(tx_buffer, "Heeee owow %d!\n", vv);
+        printf("%s\n", tx_buffer);
+        if (write(uart_fd, tx_buffer, strlen(tx_buffer)) < 0) {
+            perror("UART TX error");
+        }
+        if(++vv == 300)break;
     }
 
     // UART 포트 닫기
