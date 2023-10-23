eeeeec90b68388b4ecb8cedeb1a343089cbd6796
Ppangg <panghyun35@naver.com>
2023-10-23 15:41:18 +0900

    Serial_test1.c 실행 시 Permission Denied 나는 것 해결하기 위한 코드 추가

--- Serial_test1.c
+++ Serial_test1.c
@@ -22,6 +22,14 @@ void setup()
     printf("%s\n", "Raspberry Start!");
     fflush(stdout);
 
+    int setting = system("sudo chmod 666 /dev/ttyS0");
+
+    if(setting == -1)
+    {
+        perror("Error - Unable to change mode\n");
+        return 1;
+    }
+
     // get fd
     if((fd = serialOpen(device, baud)) < 0){
         fprintf(stderr, "Unable to open serial device : %s\n", strerror(errno));
@@ -39,13 +47,13 @@ void setup()
 void loop()
 {
     // 현재 시간 (millis) 에서 이전의 time을 뺀 값
-    if(millis()-time >= 1000)
-    {
-        printf("Hello!%d\n", millis());
-        serialPuts(fd, "Pong\n");
-        serialPutchar(fd, 65);
-        time = millis();
-    }
+    // if(millis()-time >= 1000)
+    // {
+    //     printf("Hello!%d\n", millis());
+    //     serialPuts(fd, "Pong\n");
+    //     serialPutchar(fd, 65);
+    //     time = millis();
+    // }
 
     if(serialDataAvail(fd)){ // 시리얼 포트에 사용 가능한 데이터가 있는지 확인
         printf("Hi!%d\n", millis());
